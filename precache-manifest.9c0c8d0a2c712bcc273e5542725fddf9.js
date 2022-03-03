ce45db3ca0aab54a1714fa9685063ddc907e3279
Wasnaa Raafat <wasnaaraafat@gmail.com>
2022-03-03 22:32:24 +0300

    Updates

---
+++ precache-manifest.9c0c8d0a2c712bcc273e5542725fddf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465ea1fcf30f840990b42f3504deef85",
+    "url": "/gitHubFinder/index.html"
+  },
+  {
+    "revision": "3f286127952a6c861b9b",
+    "url": "/gitHubFinder/static/css/main.bc1a1419.chunk.css"
+  },
+  {
+    "revision": "a17f37ebc75f39a0703c",
+    "url": "/gitHubFinder/static/js/2.f6e67504.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/gitHubFinder/static/js/2.f6e67504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f286127952a6c861b9b",
+    "url": "/gitHubFinder/static/js/main.1555e0db.chunk.js"
+  },
+  {
+    "revision": "b83be3d7207cf3a1edeb",
+    "url": "/gitHubFinder/static/js/runtime-main.0f01f479.js"
+  }
+]);
